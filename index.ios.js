700456d2f44877c7ee2471c5f487aa2e36837d48
Petermuturi <pmuturi65@gmail.com>
2017-01-24 13:24:43 +0300

    displays days

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment'
 import {
   AppRegistry,
   StyleSheet,
@@ -6,12 +7,28 @@ import {
   View
 } from 'react-native';
 
+import DayItem from './src/DayItem';
+
 export default class getDay extends Component {
+  days(){
+    const daysItem = [];
+
+    for (var i = 0; i < 7; i++) {
+      var day = moment().add(i, 'days').format('dddd');
+      daysItem.push(
+        <DayItem day={day} daysUntil={i} key={i} />
+      )
+    }
+    return daysItem;
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           Days of the week
+        </Text>
+        <Text>
+          {this.days()}
         </Text>
       </View>
     );
